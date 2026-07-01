58f979b09de9636a1c30e89c62658d6b40af6445
Léo Vuylsteker <leo.vuylsteker@gmail.com>
2026-07-01 14:17:00 +0200

    Initialize map

---
+++ lib/theme.ts
@@ -0,0 +1,13 @@
+// lib/theme.ts
+
+export const COLORS = {
+  ocean: "#7ec8e3",
+  land: "#e8d5a3",
+  landBorder: "#c9b686",
+  hover: "#4ade80",
+  pressed: "#22c55e",
+
+  found: "#22c55e", // pays correctement trouvé
+  wrong: "#ef4444", // mauvais clic
+  neutral: "#e8d5a3",
+};
